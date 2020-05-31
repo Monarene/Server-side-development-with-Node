c6883e7e71519e2c2b1ad62b82f9eebadbc6cdc7
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-05-31 09:20:46 +0100

    added and tested the comments section

--- Node Mongoose/index.js
+++ Node Mongoose/index.js
@@ -13,7 +13,23 @@ connect.then((db) => {
     .then((dish) => {
       console.log(dish);
 
-      return Dishes.find({}).exec();
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated Test" },
+        },
+        { new: true }
+      ).exec();
+    })
+    .then((dish) => {
+      console.log(dish);
+      dish.comments.push({
+        rating: 5,
+        comment: "I'm getting a sinking feeling!",
+        author: "Leonardo di Carpaccio",
+      });
+
+      return dish.save();
     })
     .then((dishes) => {
       console.log(dishes);
