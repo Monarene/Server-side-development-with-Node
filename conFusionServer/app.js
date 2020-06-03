1e87c01a0788bcf3a1266567d177f192bdbe6810
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-06-03 10:23:50 +0100

    bug fixes on cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -31,32 +31,31 @@ const auth = (req, res, next) => {
   if (!req.signedCookies.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
-      var err = new Error("You are not authenticated");
+      var err = new Error("You are not authenticated!");
       res.setHeader("WWW-Authenticate", "Basic");
       err.status = 401;
       next(err);
       return;
     }
-
-    var auth = new Buffer.from(
-      authHeader.split(" "[1], "base64").toString().split(";")
-    );
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
     var user = auth[0];
     var pass = auth[1];
     if (user == "admin" && pass == "password") {
       res.cookie("user", "admin", { signed: true });
-      next();
+      next(); // authorized
     } else {
-      var err = new Error("You are not authenticated");
+      var err = new Error("You are not authenticated!");
       res.setHeader("WWW-Authenticate", "Basic");
       err.status = 401;
       next(err);
     }
   } else {
-    if (req.signedCookies.user == "admin") {
+    if (req.signedCookies.user === "admin") {
       next();
     } else {
-      var err = new Error("You are not authenticated");
+      var err = new Error("You are not authenticated!");
       err.status = 401;
       next(err);
     }
